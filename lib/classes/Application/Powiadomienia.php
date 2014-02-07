c0c979d3242d6101473a147c3970b8326d5e4764
Daniel Macyszyn <daniel.macyszyn@epf.org.pl>
2014-02-07 13:20:21 +0100

    Powiadomienia

--- lib/classes/Application/Powiadomienia.php
+++ lib/classes/Application/Powiadomienia.php
@@ -6,7 +6,8 @@ class Powiadomienia extends Application
 {
 
     protected $requests_prefix = '/powiadomienia/';
-
+	private $lastSearchResponse = array();
+	
     public function getPhrases()
     {
         $ret = @$this->request('phrases');
@@ -26,18 +27,42 @@ class Powiadomienia extends Application
     public function search($queryData = array())
     {
         if (!is_null($this->user_id)) {
-            $data = $this->request('objects', $queryData, 'POST');
-            $data = @$data['search'];
-
-            foreach ($data['objects'] as &$object)
-                $object = Dane::interpretateObject($object);
-
-            return $data;
+            $this->lastSearchResponse = $this->request('objects', $queryData, 'POST');
+            return $this->lastSearchResponse;
         } else {
             return false;
         }
     }
+    
+    public function getObjects()
+    {
+    	$output = array();
 
+        if (isset($this->lastSearchResponse['search']['objects']))
+            foreach ($this->lastSearchResponse['search']['objects'] as $object)
+                $output[] = $this->interpretateObject($object);
+
+        return $output;
+    }
+	
+	public function interpretateObject($object)
+    {
+
+        $classname = ucfirst($object['dataset']);
+        $path = dirname(__FILE__);
+        if (file_exists($path . '/Dane/classes/' . $classname . '.php')) {
+            require_once($path . '/Dane/classes/' . $classname . '.php');
+            if (!class_exists('MP\\Dane\\' . $classname)) {
+                $classname = 'DataObject';
+            }
+        } else {
+            $classname = 'DataObject';
+        }
+        $classname = 'MP\\Dane\\' . $classname;
+        return new $classname($object);
+
+    }
+	
     public function markObject($object_id, $flag)
     {
         return $this->request('objects/' . $object_id . '/flag', $flag, 'POST');
