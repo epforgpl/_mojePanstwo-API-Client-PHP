8203d686c7e5d24629915f4b1ed350b0d2e2783d
Daniel Macyszyn <daniel.macyszyn@epf.org.pl>
2014-06-16 19:31:06 +0200

    Sejmometr

--- lib/classes/Application/Dane/classes/Sejm_posiedzenia.php
+++ lib/classes/Application/Dane/classes/Sejm_posiedzenia.php
@@ -10,7 +10,12 @@ class Sejm_posiedzenia extends DocDataObject
         'date' => 'data_start',
         'label' => 'label'
     );
+	
+	protected $schema = array(
+		array('data_start', ''),
+	);
 
+	
     public function getLabel()
     {
         return 'Posiedzenie Sejmu';
@@ -18,7 +23,15 @@ class Sejm_posiedzenia extends DocDataObject
 
     public function getTitle()
     {
-        return 'Posiedzenie Sejmu nr ' . $this->getData('numer');
+    	if( $this->getData('numer') )
+	        return 'Posiedzenie Sejmu nr ' . $this->getData('numer');
+	    else
+	    	return $this->getData('tytul');
+    }
+    
+    public function getShortTitle()
+    {
+        return $this->getTitle();
     }
     
     public function hasHighlights()
