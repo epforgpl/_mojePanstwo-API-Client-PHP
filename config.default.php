198a420f9d3eda238be36a39a39eaacdefb06550
kangoor@skrzynka.pl <kangoor@skrzynka.pl>
2014-06-17 14:01:37 +0200

    TODO

--- config.default.php
+++ config.default.php
@@ -3,3 +3,10 @@
 const API_PROTOCOL = 'http://';
 const API_HOST = 'mpapi.pl';
 const API_FORMAT = 'json';
+
+// TODO URL_PROTOCOL and URL_HOST should be specified as initialization parameters
+// TODO powinny wyleciec stad, bo to nie client API tworzy sciezki
+const URL_PROTOCOL = 'http://';
+const URL_HOST = 'THIS_SERVER_ADDRESS';
+
+const MPAPI_DEBUG = 0;
