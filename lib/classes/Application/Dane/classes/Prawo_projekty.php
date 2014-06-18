6cb9475cdd7def5292d599a4a2953f8e1c497564
Daniel Macyszyn <daniel.macyszyn@epf.org.pl>
2014-06-18 09:52:49 +0200

    Sejmometr

---
+++ lib/classes/Application/Dane/classes/Prawo_projekty.php
@@ -0,0 +1,45 @@
+<?
+
+namespace MP\Dane;
+
+class Prawo_projekty extends DocDataObject
+{
+	
+	protected $schema = array(
+		array('autorzy_html', ''),
+		array('opis', 'Opis', 'string', array(
+			'truncate' => 120,
+		)),
+		array('status_str', 'Status'),
+		array('data', 'Data', 'date')
+	);
+	
+    protected $routes = array(
+        'title' => 'tytul',
+        'shortTitle' => 'tytul_skrocony',
+        'date' => 'data_start',
+        'label' => 'label',
+        'description' => 'opis',
+    );
+    
+    protected $hl_fields = array(
+    	'autorzy_html', 'status_str'
+    );
+
+    public function getLabel()
+    {
+        switch( $this->getData('typ_id') ) {
+	        case '1': return 'Projekt ustawy';
+	        case '2': return 'Projekt uchwały';
+	        case '5': return 'Powołanie / odwołanie';
+	        case '6': return 'Umowa międzynarodowa';
+	        case '11': return 'Sprawozdanie kontrolne';
+	        case '12': return 'Projekt';
+	        case '100': return 'Zmiana w składach komisji';
+	        case '103': return 'Wniosek o referendum';
+        }
+    }
+    
+    public $force_hl_fields = true;
+
+}
