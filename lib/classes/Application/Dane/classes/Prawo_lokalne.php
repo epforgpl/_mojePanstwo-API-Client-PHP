3dd977256608d4c5955cfb6d67d4042fa6a2f258
Daniel Macyszyn <daniel.macyszyn@epf.org.pl>
2014-07-25 16:33:35 +0200

    Gminy prawo_lokalne url fix

--- lib/classes/Application/Dane/classes/Prawo_lokalne.php
+++ lib/classes/Application/Dane/classes/Prawo_lokalne.php
@@ -17,6 +17,11 @@ class Prawo_lokalne extends DocDataObject
     {
         return 'Uchwała <strong>' . $this->getData('jednostka_dopelniacz') . '</strong>, numer <strong>' . $this->getData('akt_numer') . '</strong> z dnia <strong>' . $this->dataSlownie($this->getDate()) . '</strong>';
     }
+    
+    public function getUrl()
+    {
+	    return '/dane/gminy/' . $this->getData('gmina_id') . '/rada_uchwaly/' . $this->getId();
+    }
 	
 	public $force_hl_fields = true;
 	
