679f6a488d879f29395eace7a2e44757be2e3a0d
Daniel Macyszyn <daniel.macyszyn@epf.org.pl>
2014-09-15 13:18:19 +0200

    Ustawy

--- lib/classes/Application/Dane/classes/Ustawy.php
+++ lib/classes/Application/Dane/classes/Ustawy.php
@@ -6,25 +6,25 @@ class Ustawy extends DataObject
 {
 	
 	protected $schema = array(
-		array('status_id', 'Status', 'string', array(
+		array('prawo.status_id', 'Status', 'string', array(
 			'dictionary' => array(
 				'1' => 'Obowiązująca',
 				'2' => 'Nieobowiązująca',
 			),
 		)),
-		array('data_wydania', 'Data wydania'),
-		array('data_publikacji', 'Data publikacji'),
-		array('data_wejscia_w_zycie', 'Data wejścia w życie'),
+		array('prawo.data_wydania', 'Data wydania'),
+		array('prawo.data_publikacji', 'Data publikacji'),
+		array('prawo.data_wejscia_w_zycie', 'Data wejścia w życie'),
 	);
 	
     protected $routes = array(
-        'title' => 'tytul',
-        'shortTitle' => 'tytul_skrocony',
+        'title' => 'prawo.tytul',
+        'shortTitle' => 'prawo.tytul_skrocony',
         'date' => false,
     );
     
     protected $hl_fields = array(
-    	'status_id', 'data_wydania', 'data_publikacji', 'data_wejscia_w_zycie'
+    	'prawo.status_id', 'prawo.data_wydania', 'prawo.data_publikacji', 'prawo.data_wejscia_w_zycie'
     );
 
     public function getLabel()
@@ -34,7 +34,7 @@ class Ustawy extends DataObject
     
     public function getFullLabel()
     {
-        return 'Ustawa z dnia ' . dataSlownie( $this->getData('data_wydania') );
+        return 'Ustawa z dnia ' . dataSlownie( $this->getData('prawo.data_wydania') );
     }
 
 }
